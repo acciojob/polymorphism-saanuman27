33c1503aba1b50bbfbe70cd4a0bcbbde2be9fb04
Suraj Anumandla <rajsuraj926@gmail.com>
2022-10-10 23:50:17 +0530

    PolyMorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,21 +4,22 @@ public class Main {
     public static void main(String args[]) {
 
         Product p = new Product();
-        System.out.println(p.product(2,3));
-        System.out.println(p.product(2,3,5));
-        System.out.println(p.product(4.0,6.0));
-    }
-}
-class Product{
+        System.out.println(p.product(2,3) +" " + p.product(2,3,5) + " " +p.product(4.0,5.1));
 
-    public int product(int x, int y){
-        return x+y;
-    }
-    public int product(int x, int y, int z) {
-        return x +y+z;
     }
 
-    public double product(double x, double y) {
-        return x+y;
+    public static class Product {
+
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
     }
 }
